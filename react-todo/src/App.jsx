ecf658a63bd4e2caf64231def4965d1afd053f1b
jjh319 <136777270+jjh319@users.noreply.github.com>
2024-04-16 22:26:18 +0900

    로컬 스토리지 데이터 조회 함수 구현

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,20 +1,31 @@
 import { useState } from 'react';
 
+function fetchTodos() {
+    const result = [];
+    for (let i = 0; i < localStorage.length; i++) {
+        const value = localStorage.key(i);
+        result.push(value);
+    } // for
+
+    return result;
+} // fetchTodos
+
 function App() {
     // const [count, setCount] = useState(0);
     const [inputText, setInputText] = useState('');
     // state = 컴포넌트가 기억해야 할 특정 값
+    const todos = fetchTodos();
 
     const handleInput = (event) => {
         console.log(event);
         const value = event.target.value;
         setInputText(value);
-    };
+    }; // handleInput
 
     const handleClick = () => {
         console.log('clicked');
         localStorage.setItem(inputText, inputText);
-    };
+    }; // handleClick
 
     return (
         <div>
@@ -23,8 +34,15 @@ function App() {
                 <input type="text" value={inputText} onChange={handleInput} />
                 <button onClick={handleClick}>add</button>
             </div>
+            <div>
+                <ul>
+                    <li>할 일 추가</li>
+                    <li>할 일 조회</li>
+                    <li>할 일 삭제</li>
+                </ul>
+            </div>
         </div>
-    );
-}
+    ); // return
+} // App
 
 export default App;
