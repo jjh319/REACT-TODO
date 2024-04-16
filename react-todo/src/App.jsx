7caa651ab94fe4b3362807aeed2340ca951f653e
jjh319 <136777270+jjh319@users.noreply.github.com>
2024-04-16 22:44:42 +0900

    할 일 목록을 화면에 표시하기

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,48 +1,48 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 function fetchTodos() {
-    const result = [];
-    for (let i = 0; i < localStorage.length; i++) {
-        const value = localStorage.key(i);
-        result.push(value);
-    } // for
+  const result = [];
+  for (let i = 0; i < localStorage.length; i++) {
+    const value = localStorage.key(i);
+    result.push(value);
+  } // for
 
-    return result;
+  return result;
 } // fetchTodos
 
 function App() {
-    // const [count, setCount] = useState(0);
-    const [inputText, setInputText] = useState('');
-    // state = 컴포넌트가 기억해야 할 특정 값
-    const todos = fetchTodos();
+  // const [count, setCount] = useState(0);
+  const [inputText, setInputText] = useState("");
+  // state = 컴포넌트가 기억해야 할 특정 값
+  const todos = fetchTodos();
 
-    const handleInput = (event) => {
-        console.log(event);
-        const value = event.target.value;
-        setInputText(value);
-    }; // handleInput
+  const handleInput = (event) => {
+    console.log(event);
+    const value = event.target.value;
+    setInputText(value);
+  }; // handleInput
 
-    const handleClick = () => {
-        console.log('clicked');
-        localStorage.setItem(inputText, inputText);
-    }; // handleClick
+  const handleClick = () => {
+    console.log("clicked");
+    localStorage.setItem(inputText, inputText);
+  }; // handleClick
 
-    return (
-        <div>
-            <h1>TODO 앱</h1>
-            <div>
-                <input type="text" value={inputText} onChange={handleInput} />
-                <button onClick={handleClick}>add</button>
-            </div>
-            <div>
-                <ul>
-                    <li>할 일 추가</li>
-                    <li>할 일 조회</li>
-                    <li>할 일 삭제</li>
-                </ul>
-            </div>
-        </div>
-    ); // return
+  return (
+    <div>
+      <h1>TODO 앱</h1>
+      <div>
+        <input type="text" value={inputText} onChange={handleInput} />
+        <button onClick={handleClick}>add</button>
+      </div>
+      <div>
+        <ul>
+          {todos.map((todo, index) => {
+            return <li key={index}>{todo}</li>;
+          })}
+        </ul>
+      </div>
+    </div>
+  ); // return
 } // App
 
 export default App;
