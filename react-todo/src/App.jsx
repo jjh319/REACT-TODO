71e48f377c4cc30792d0be20dd1e4cf73377b0d3
jjh319 <136777270+jjh319@users.noreply.github.com>
2024-04-20 20:32:09 +0900

    리스트 컴포넌트 생성 및 표시

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,51 +1,39 @@
 import { useState } from "react";
 import TodoHeader from "./components/TodoHeader";
 import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
 
-function fetchTodos() {
-  const result = [];
-  for (let i = 0; i < localStorage.length; i++) {
-    const value = localStorage.key(i);
-    result.push(value);
-  } // for
+// function fetchTodos() {
+//   const result = [];
+//   for (let i = 0; i < localStorage.length; i++) {
+//     const value = localStorage.key(i);
+//     result.push(value);
+//   } // for
 
-  return result;
-} // fetchTodos
+//   return result;
+// } // fetchTodos
 
 function App() {
-  const [todos, setTodos] = useState(fetchTodos());
+  // const [todos, setTodos] = useState(fetchTodos());
 
-  const handleRemove = (todo, index) => {
-    // console.log(todo, index);
-    // todos.splice(index, 1); // splice = 배열의 몇번째 인덱스값에서 몇개를 지우겠다.
-    const result = todos.filter((todoItem) => {
-      if (todoItem !== todo) {
-        return true;
-      } // if
-    }); // filter
+  // const handleRemove = (todo, index) => {
+  //   // console.log(todo, index);
+  //   // todos.splice(index, 1); // splice = 배열의 몇번째 인덱스값에서 몇개를 지우겠다.
+  //   const result = todos.filter((todoItem) => {
+  //     if (todoItem !== todo) {
+  //       return true;
+  //     } // if
+  //   }); // filter
 
-    setTodos(result);
-    localStorage.removeItem(todo);
-  };
+  //   setTodos(result);
+  //   localStorage.removeItem(todo);
+  // };
 
   return (
     <div>
       <TodoHeader />
       <TodoInput />
-      <div>
-        <ul>
-          {todos.map((todo, index) => {
-            return (
-              <li key={index}>
-                <span>{todo}</span>
-                <button onClick={() => handleRemove(todo, index)}>
-                  remove
-                </button>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
+      <TodoList />
     </div>
   ); // return
 } // App
---
+++ react-todo/src/components/TodoList.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+function fetchTodos() {
+  const result = [];
+  for (let i = 0; i < localStorage.length; i++) {
+    const value = localStorage.key(i);
+    result.push(value);
+  } // for
+
+  return result;
+} // fetchTodos
+
+function TodoList() {
+  const [todos, setTodos] = useState(fetchTodos());
+
+  const handleRemove = (todo, index) => {
+    // console.log(todo, index);
+    // todos.splice(index, 1); // splice = 배열의 몇번째 인덱스값에서 몇개를 지우겠다.
+    const result = todos.filter((todoItem) => {
+      if (todoItem !== todo) {
+        return true;
+      } // if
+    }); // filter
+
+    setTodos(result);
+    localStorage.removeItem(todo);
+  };
+
+  return (
+    <div>
+      <ul>
+        {todos.map((todo, index) => {
+          return (
+            <li key={index}>
+              <span>{todo}</span>
+              <button onClick={() => handleRemove(todo, index)}>remove</button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default TodoList;
