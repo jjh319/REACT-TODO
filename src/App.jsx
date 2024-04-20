ef884455aff2be49b43231123f1b47489b318abe
jjh319 <136777270+jjh319@users.noreply.github.com>
2024-04-20 22:07:24 +0900

    branch 나누기

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import TodoHeader from "./components/TodoHeader";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
+
+function fetchTodos() {
+  const result = [];
+  for (let i = 0; i < localStorage.length; i++) {
+    const value = localStorage.key(i);
+    result.push(value);
+  } // for
+
+  return result;
+} // fetchTodos
+
+function App() {
+  const [todos, setTodos] = useState(fetchTodos());
+
+  const addTodo = (todo) => {
+    localStorage.setItem(todo, todo);
+    // setInputText("");
+    // todos.push(inputText);
+    // 배열 상태 추가 방식
+    setTodos((currentTodos) => {
+      return [...currentTodos, todo];
+    });
+  }; // handleClick
+
+  const removeTodo = (todo, index) => {
+    // console.log(todo, index);
+    // todos.splice(index, 1); // splice = 배열의 몇번째 인덱스값에서 몇개를 지우겠다.
+    const result = todos.filter((todoItem) => {
+      if (todoItem !== todo) {
+        return true;
+      } // if
+    }); // filter
+
+    setTodos(result);
+    localStorage.removeItem(todo);
+  };
+
+  return (
+    <div>
+      <TodoHeader />
+      <TodoInput onTodoAdd={addTodo} />
+      <TodoList todos={todos} onTodoRemove={removeTodo} />
+    </div>
+  ); // return
+} // App
+
+export default App;
---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+function TodoInput({ onTodoAdd }) {
+  const [inputText, setInputText] = useState("");
+
+  const handleInput = (event) => {
+    const value = event.target.value;
+    setInputText(value);
+  }; // TodoInput
+
+  const handleClick = () => {
+    onTodoAdd(inputText);
+    setInputText("");
+  }; // handleClick
+
+  return (
+    <div>
+      <input type="text" value={inputText} onChange={handleInput} />
+      <button onClick={handleClick}>add</button>
+    </div>
+  );
+}
+
+export default TodoInput;
